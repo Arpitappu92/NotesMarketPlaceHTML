e39765b1c20439bc7ad739f808ad7cabca636a77
Arpitappu92 <36881829+Arpitappu92@users.noreply.github.com>
2021-03-07 20:46:17 +0530

    MVC

---
+++ Notes-MarketPlace/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Notes_MarketPlace.Models;
+
+
+namespace Notes_MarketPlace.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NotesEntities objNotesEntities = new NotesEntities();
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        public ActionResult SearchNotes()
+        {
+            ViewBag.Country = objNotesEntities.Country.Where(x => x.IsActive == true);
+            ViewBag.NoteType = objNotesEntities.NoteTypes.Where(x => x.IsActive == true);
+            ViewBag.NoteCategory = objNotesEntities.NoteCategories.Where(x => x.IsActive == true);
+
+            List<SellerNotes> sn = objNotesEntities.SellerNotes.ToList();
+            return View(sn);
+        }
+
+        [HttpGet]
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ContactUs(ContactUsModel obj)
+        {
+            if(ModelState.IsValid)
+            {
+                ContactUs cu = new ContactUs
+                {
+                    FullName = obj.FullName,
+                    EmailId = obj.EmailId, 
+                    Subject = obj.Subject,
+                    Question = obj.Question,
+                    CreatedDate = DateTime.Now,
+                    IsActive = false
+                };
+                if(User.Identity.IsAuthenticated)
+                {
+                    cu.IsActive = true;
+                }
+
+                objNotesEntities.ContactUs.Add(cu);
+                objNotesEntities.SaveChanges();
+            }
+
+            AccRelTemplate.ContactUsEmail.ContactUs(obj.Subject, obj.FullName, obj.Question);
+
+            ModelState.Clear();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Notes-MarketPlace/Helpers/EncryptPasswords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+
+namespace Notes_MarketPlace.Helpers
+{
+    public class EncryptPasswords
+    {
+        public static string EncryptPasswordMd5(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding encoder = new UTF8Encoding();
+            Byte[] originalBytes = encoder.GetBytes(password);
+            Byte[] encodedBytes = md5.ComputeHash(originalBytes);
+            var hashedPassword = BitConverter.ToString(encodedBytes).Replace("-", "");
+            var newPassword = hashedPassword.ToLower();
+
+            return newPassword;
+
+        }
+    }
+}
